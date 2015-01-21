e8e53f1c4d70f6b7bcf3244f672aab0b5205bfbc
pstrawberrie <pstrawberriedev@gmail.com>
2015-01-20 17:05:03 -0700

    Nav rework / Individual page start

--- js/base.js
+++ js/base.js
@@ -2,20 +2,33 @@
 //	Alien Kitty
 //-------------------------
 
-$("#slide-nav a").click(function() {
+$("#slide-nav .button").each(function() {
 
-	var intro = $("#intro-kitty");
-	var kitty = $("#alien-kitty");
-	var nav = $("#slide-nav");
-	var bubbles = $("#intro-kitty .bubble-git, #intro-kitty .bubble-info");
-	var currentPage = "#intro-kitty";
-	currentPage = $(this).attr('id');
-	
-	if(intro.is(':visible')) {
-		intro.slideUp(function() {
-			nav.addClass("morph");
-		});
-	}
+        var intro = $("#intro-kitty");
+        var kitty = $("#alien-kitty");
+        var nav = $("#slide-buttons");
+        var bubbles = $("#intro-kitty .bubble-git, #intro-kitty .bubble-info");
+        var currentID ="#" + $(this).attr('id');
+        var currentPage = $("#content").find(currentID);
+
+    $(this).click(function() {
+        $("#slide-nav .button").removeClass("active");
+        if($(this).attr('id') != "introduction") { intro.slideUp(); } 
+        else { intro.slideDown(); }
+        
+        if(! $(this).hasClass("active")) {$(this).addClass("active");}
+        
+        if(currentPage.is(':visible')) {
+            return;
+        }
+         else {
+            $("#content .page").hide(function() {
+                currentPage.slideDown();
+            });
+        }
+        
+        
+    });
 	
 	
 });
